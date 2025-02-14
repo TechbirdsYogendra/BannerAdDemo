8bdda08dbfb0433f320559617c46b613a1a6e1b0
Yogendra Singh <000yogi1990@gmail.com>
2025-02-14 12:28:16 +0530

    Initial Commit

---
+++ BannerAdDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BannerAdDemo
+//
+//  Created by Yogendra Singh on 14/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BannerAdDemoTests/BannerAdDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BannerAdDemoTests.swift
+//  BannerAdDemoTests
+//
+//  Created by Yogendra Singh on 14/02/25.
+//
+
+import Testing
+@testable import BannerAdDemo
+
+struct BannerAdDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
